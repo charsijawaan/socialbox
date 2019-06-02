0b9c2fe1bb3a3894de10c98ea22dc04d808dee13
usama <shakeelusama3@gmail.com>
2019-06-02 16:47:58 +0500

    db connect try 3

--- db_modules/mongoose.js
+++ db_modules/mongoose.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb+srv://socialbox:Createyour1@cluster0-pw7nl.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
+mongoose.connect("mongodb://socialbox:<password>@cluster0-shard-00-00-pw7nl.mongodb.net:27017,cluster0-shard-00-01-pw7nl.mongodb.net:27017,cluster0-shard-00-02-pw7nl.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", { useNewUrlParser: true });
 // mongoose.connect('mongodb://localhost:27017/socialbox', { useNewUrlParser: true });
 
 module.exports = { mongoose };
