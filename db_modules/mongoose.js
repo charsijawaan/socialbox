f3eb2ec00bb4d27e89e58105a20f79d9b4dd9b4e
usama <shakeelusama3@gmail.com>
2019-06-02 16:26:12 +0500

    db connect 2

--- db_modules/mongoose.js
+++ db_modules/mongoose.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://socialbox:Createyour1@cluster0-pw7nl.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://socialbox:Createyour1@cluster0-pw7nl.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true });
 // mongoose.connect('mongodb://localhost:27017/socialbox', { useNewUrlParser: true });
 
 module.exports = { mongoose };
