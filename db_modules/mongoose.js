06f0aa7da068e46e9b5e7c2131cff1a202e8a3a7
usama <shakeelusama3@gmail.com>
2019-06-02 17:15:11 +0500

    db cloud 4 try

--- db_modules/mongoose.js
+++ db_modules/mongoose.js
@@ -1,7 +1,11 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://socialbox:<password>@cluster0-shard-00-00-pw7nl.mongodb.net:27017,cluster0-shard-00-01-pw7nl.mongodb.net:27017,cluster0-shard-00-02-pw7nl.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority", { useNewUrlParser: true });
-// mongoose.connect('mongodb://localhost:27017/socialbox', { useNewUrlParser: true });
+mongoose.connect("mongodb+srv://socialbox:Createyour1@cluster0-pw7nl.mongodb.net/test?retryWrites=true&w=majority", { useNewUrlParser: true }).then(() => {
+    console.log("Connected to Database");
+});
+// mongoose.connect('mongodb://localhost:27017/socialbox', { useNewUrlParser: true }).then(() => {
+//     console.log('Connected to Database');
+// });
 
 module.exports = { mongoose };
